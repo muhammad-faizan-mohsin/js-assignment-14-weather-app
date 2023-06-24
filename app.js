48e0f970aa1d045ebf7e2f57cee513f68cb05813
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-06-24 19:54:53 +0500

    90%

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ let currentweather = () => {
     navigator.geolocation.getCurrentPosition((location) => {
         let long = location.coords.longitude
         let lat = location.coords.latitude
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&exclude={part}&appid=e3b38e7f14164a9bccbcc2675ed01e02&units=metric`)
-            .then((res) => res.json())
-            .then((res) => {
-           
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&exclude={part}&appid=e3b38e7f14164a9bccbcc2675ed01e02&units=metric`)
+                .then((res) => res.json())
+                .then((res) => {
+            
                 locations.innerHTML = res.name
                 deg.innerHTML = `${Math.round(res.main.temp)}°`
                 main.innerHTML = res.weather[0].main
@@ -27,6 +27,10 @@ let currentweather = () => {
                     iconImg.src = "img/cloudy.svg"
                     bggif.style.backgroundImage = "url('./img/clouds.gif')"
                 } 
+                else if (res.weather[0].main == "Drizzle") {
+                    iconImg.src = "img/rain.svg"
+                    bggif.style.backgroundImage = "url('./img/drizzle.gif')"
+                }
                 else if (res.weather[0].main == "Rain") {
                     iconImg.src = "img/rain.svg"
                     bggif.style.backgroundImage = "url('./img/rain.gif')"
@@ -55,10 +59,13 @@ let currentweather = () => {
                     iconImg.src = "img/smoke.svg"
                     bggif.style.backgroundImage = "url('./img/smoke.gif')"
                 }
-                console.log(res)
+              console.log(res)
             })
             .catch((err) => {
-                console.log(err)
+                Swal.fire({
+                    icon: 'error',
+                    text: 'Your email or password are incorrect ',
+                  });        
             })
     }   )
 }
@@ -66,15 +73,79 @@ currentweather()
 
 
 let searchweather = () => {
+
+        
+if(cityinput.value.trim() == ""){
+console.log("error")
+}
+else{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityinput.value}&appid=e3b38e7f14164a9bccbcc2675ed01e02&units=metric`)
     .then((res) => res.json())
     .then((res) => {
-        
-if(cityinput.value == res.name){
-  console.log("hello")
+        locations.innerHTML = res.name
+        deg.innerHTML = `${Math.round(res.main.temp)}°`
+        main.innerHTML = res.weather[0].main
+        humidity.innerHTML = res.main.humidity + "%"
+        clouds.innerHTML = res.clouds.all + "%"
+        wind.innerHTML = res.wind.speed + "km/h"
+
+        if (res.weather[0].main == "Clouds") {
+            iconImg.src = "img/cloudy.svg"
+            bggif.style.backgroundImage = "url('./img/clouds.gif')"
+        } 
+        else if (res.weather[0].main == "Drizzle") {
+            iconImg.src = "img/rain.svg"
+            bggif.style.backgroundImage = "url('./img/drizzle.gif')"
+        }
+        else if (res.weather[0].main == "Rain") {
+            iconImg.src = "img/rain.svg"
+            bggif.style.backgroundImage = "url('./img/rain.gif')"
+        }
+        else if (res.weather[0].main == "Clear") {
+            iconImg.src = "img/clear-day.svg"
+            bggif.style.backgroundImage = "url('./img/clear.gif')"
+        }
+        else if (res.weather[0].main == "Snow") {
+            iconImg.src = "img/snow.svg"
+            bggif.style.backgroundImage = "url('./img/snow.gif')"
+        }   
+        else if (res.weather[0].main == "Haze") {
+            iconImg.src = "img/haze.svg"
+            bggif.style.backgroundImage = "url('./img/haze.gif')"
+        } 
+        else if (res.weather[0].main == "Fog") {
+            iconImg.src = "img/fog.svg"
+            bggif.style.backgroundImage = "url('./img/fog.gif')"
+        } 
+        else if (res.weather[0].main == "Dust") {
+            iconImg.src = "img/dust.svg"
+            bggif.style.backgroundImage = "url('./img/dust.gif')"
+        } 
+        else if (res.weather[0].main == "Smoke") {
+            iconImg.src = "img/smoke.svg"
+            bggif.style.backgroundImage = "url('./img/smoke.gif')"
+        }
+        console.log(res)
+    })
+    .catch((err) => {
+        Swal.fire({
+            icon: 'error',
+            text: 'Invalid Country or City Name',
+          });  
+          currentweather()
+    })
+    }
 }
-})
-.catch((err) => {
-    console.log(err)
-})
+
+let forcast = () => {
+  
+    fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=1&appid=e3b38e7f14164a9bccbcc2675ed01e02`)
+    .then((res) => res.json())
+    .then((res) => {
+console.log(res)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 }
+forcast()
